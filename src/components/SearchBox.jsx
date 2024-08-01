c3799a78adaaca6c42d6ed6713cc592028964d04
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2024-08-01 10:48:48 +0530

    Header Create

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,8 +1,14 @@
+import { IoMdSearch } from "react-icons/io";
 
 export const SearchBox = () => {
   return (
     <>
-<input type="text"  className="outline-none bg-[#5a5a5a] w-72 h-7 p-2 rounded-md text-white " placeholder="Search "/>
+    <div className="flex items-center bg-[#5a5a5a] w-72 h-7 px-2  rounded-md ">
+        
+        <input type="text"  className="  outline-none w-full h-7  text-white bg-transparent " placeholder="Search "/>
+        <IoMdSearch  className="text-white text-lg "/>
+        </div>
+
     </>
   )
 }
--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -6,7 +6,7 @@ import NavListdata from "../data/NavListdata";
 export const Header = () => {
   return (
     <div>
-      <header className="w-[1400px] h-16 flex justify-between m-auto items-center">
+      <header className="max-w-[1400px] flex-wrap h-16 flex justify-between m-auto items-center">
         <a href="/" className="text-[#EAE6DE] font-bold text-4xl bg-blue-500 p-1 rounded-sm">Prime Movies</a>
         <ul className="flex gap-10 font-bold text-[#EAE6DE] text-lg  uppercase">
           {NavListdata.map((nav) => (
