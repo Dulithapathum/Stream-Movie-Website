d7ca5f078c98aa1af1c856a6bb2b4ec9ea71ee33
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2024-08-01 15:44:37 +0530

    Show Image In Top Movie Section

--- src/pages/TopMovies.jsx
+++ src/pages/TopMovies.jsx
@@ -23,7 +23,17 @@ useEffect(()=>{
 
   return (
     <div className="w-[1200px] m-auto ">
-      
+        <h1 className="text-yellow-300 font-semibold mt-8 text-3xl text-center uppercase">Top Movies</h1>
+        <center><hr className=" w-20  border-t-4 border-yellow-400 mt-2  " /></center>
+        <div className="flex w-full flex-wrap  justify-evenly">
+        {topMovies.slice(0, 14).map((movie)=>{
+            return <>
+            <div>
+                <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} className="w-40" />
+            </div>
+            </>
+        })}
+        </div>
        
     </div>
   )
