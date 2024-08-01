1b460067f81dcf2d6ffc1ae8541124996c314323
Dulitha Pathum <141697422+Dulithapathum@users.noreply.github.com>
2024-08-01 13:02:12 +0530

    Fix Error:header search box

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -3,9 +3,9 @@ import { IoMdSearch } from "react-icons/io";
 export const SearchBox = () => {
   return (
     <>
-    <div className="flex items-center bg-[#5a5a5a] w-72 h-7 px-2  rounded-md ">
+    <div className="flex items-center bg-[#5a5a5a] w-72 h-9 px-2  rounded-md ">
         
-        <input type="text"  className="  outline-none w-full h-7  text-white bg-transparent " placeholder="Search "/>
+        <input type="text"  className="  outline-none w-full h-full  text-white bg-transparent " placeholder="Search "/>
         <IoMdSearch  className="text-white text-lg "/>
         </div>
 
